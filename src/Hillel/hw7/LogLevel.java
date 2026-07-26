d1fae511aa341ceb0adb2ed040588fda3375ebfb
Alina-Sakidon <alinasakidon@gmail.com>
2026-07-26 21:02:26 +0300

    Add hw7

---
+++ src/Hillel/hw7/LogLevel.java
@@ -0,0 +1,18 @@
+package Hillel.hw7;
+
+public enum LogLevel {
+    INFO(ConsoleColours.GREEN),
+    DEBUG(ConsoleColours.BLUE),
+    WARN(ConsoleColours.YELLOW),
+    ERROR(ConsoleColours.RED);
+
+    private final String colour;
+
+    LogLevel(String colour) {
+        this.colour = colour;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+}
