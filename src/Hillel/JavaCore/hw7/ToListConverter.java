a0b21a50995ec8e6313cfd0092421efa46c9c71d
Alina-Sakidon <alinasakidon@gmail.com>
2026-07-30 11:56:52 +0300

    JAVA CORE Add hw7

---
+++ src/Hillel/JavaCore/hw7/ToListConverter.java
@@ -0,0 +1,36 @@
+package Hillel.JavaCore.hw7;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+/*2. Створити метод toList, що приймає на вхід цілісний масив довільної довжини.
+Конвертувати масив у список відповідного типу даних та повернути з методу.
+        Наприклад:
+
+Було Array [1, 2, 3]
+
+Стало List [1, 2, 3]*/
+
+public class ToListConverter {
+    public static void main(String[] args) {
+        int[] randomNumbers = new int[5];
+        Random random = new Random();
+        for (int i = 0; i < randomNumbers.length; i++) {
+            randomNumbers[i] = random.nextInt(20);
+        }
+        System.out.println("Array: " + Arrays.toString(randomNumbers));
+
+        System.out.println("List: " + toList(randomNumbers));
+    }
+
+    public static List<Integer> toList(int[] numbers) {
+        List<Integer> list = new ArrayList<>();
+
+        for (int num : numbers) {
+            list.add(num);
+        }
+        return list;
+    }
+}
---
+++ src/Hillel/JavaCore/hw7/phonebook/Note.java
@@ -0,0 +1,26 @@
+package Hillel.JavaCore.hw7.phonebook;
+
+import java.util.List;
+
+public class Note {
+    private String name;
+    private List<String> phoneNumber;
+
+    public Note(String name, List<String> phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Note{" +
+                "name='" + name + '\'' +
+                ", phoneNumber=" + phoneNumber +
+                '}';
+    }
+}
