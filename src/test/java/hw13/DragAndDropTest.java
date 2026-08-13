1b1e6b6d95dc3af10067705381b9883cd55dc708
Alina-Sakidon <alinasakidon@gmail.com>
2026-08-13 19:19:06 +0300

    add hw13

---
+++ src/test/java/hw13/DragAndDropTest.java
@@ -0,0 +1,29 @@
+package hw13;
+
+import hw12.BaseTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import javax.swing.*;
+
+public class DragAndDropTest extends BaseTest {
+
+    @Test
+    public void drugAndDrop(){
+        driver.get("https://crossbrowsertesting.github.io/drag-and-drop.html");
+        driver.manage().window().maximize();
+
+        WebElement source = driver.findElement(By.id("draggable"));
+        WebElement target = driver.findElement(By.id("droppable"));
+
+        new Actions(driver).dragAndDrop(source, target).perform();
+
+        String messageToCheck = driver.findElement(By.id("droppable")).getText();
+
+        Assert.assertEquals(messageToCheck, "Dropped!");
+    }
+}
